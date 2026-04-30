29d87bf792d6b36cfaeca24a4e12a4809fede95c
ajeetsm2010 <ajeetsm20@gmail.com>
2026-04-30 13:59:56 +0530

    Update intermediate_queries.sql

--- intermediate_queries.sql
+++ intermediate_queries.sql
@@ -32,3 +32,30 @@ SELECT
     END
 FROM BST
 ORDER BY N;
+
+
+-- =========================================
+-- 4: New Companies
+-- Topic: Joins + Aggregation
+-- =========================================
+
+SELECT c.company_code,
+       c.founder,
+       COUNT(DISTINCT lm.lead_manager_code) AS total_lead_managers,
+       COUNT(DISTINCT sm.senior_manager_code) AS total_senior_managers,
+       COUNT(DISTINCT m.manager_code) AS total_managers,
+       COUNT(DISTINCT e.employee_code) AS total_employees
+FROM Company c
+LEFT JOIN Lead_Manager lm 
+    ON c.company_code = lm.company_code
+LEFT JOIN Senior_Manager sm 
+    ON lm.lead_manager_code = sm.lead_manager_code
+LEFT JOIN Manager m 
+    ON sm.senior_manager_code = m.senior_manager_code
+LEFT JOIN Employee e 
+    ON m.manager_code = e.manager_code
+GROUP BY c.company_code, c.founder
+ORDER BY c.company_code;
+
+
+
