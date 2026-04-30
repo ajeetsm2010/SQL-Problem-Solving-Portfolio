2ab8223d5051a0b4d1d82fe34ff30198de776734
ajeetsm2010 <ajeetsm20@gmail.com>
2026-04-30 13:58:45 +0530

    Update advanced_queries.sql

--- advanced_queries.sql
+++ advanced_queries.sql
@@ -37,17 +37,6 @@ FROM
 GROUP BY rn;
 
 
--- 3. Binary Tree Nodes
-
-SELECT 
-    N,
-    CASE
-        WHEN P IS NULL THEN 'Root'
-        WHEN N NOT IN (SELECT P FROM BST WHERE P IS NOT NULL) THEN 'Leaf'
-        ELSE 'Inner'
-    END
-FROM BST
-ORDER BY N;
 
 
 
